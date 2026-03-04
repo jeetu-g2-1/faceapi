d5dee534ac4b0deca6f1e590e099bbbedad6ea8a
jeetu_g2_1 <jithendrakumar212004@gmail.com>
2026-03-04 16:34:27 +0530

    update views.py

--- api1/views.py
+++ api1/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from api1.authentication import APIKeyAuthentication
 from api1.logic import compute_face_distance
-from api1.models import FaceImageResponse
+#from api1.models import FaceImageResponse
 
 class FaceVerificationAPI(APIView):
     authentication_classes = [APIKeyAuthentication]
